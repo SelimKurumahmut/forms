3f73f77497a09e7e84e6576ef7fee721ebc91349
Selim Kurumahmut <1700002324@stu.iku.edu.tr>
2021-03-20 10:58:15 +0300

    The error in the input spelling has been fixed.

--- form1.php
+++ form1.php
@@ -1,7 +1,7 @@
 <h1>HTML Forms</h1>
 <form method="GET">
     <p><label for="input">User input</label>
-        <input type="text" name="other" size="40">
+        <input type="text" name="other" size="40"/>
     </p>
-    <input type="submit">
+    <input type="submit"/>
 </form>
